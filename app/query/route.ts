6032577046221c2e8d512a2a1eb2d23967fb22b2
Miguel Ángel López <mangell0624@gmail.com>
2025-09-22 12:17:54 +0200

    Update route.ts

--- app/query/route.ts
+++ app/query/route.ts
@@ -3,20 +3,21 @@ import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
-const data = await sql`
+  const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE invoices.amount = 666;
   `;
 
-return data;
+  return data;
 }
 
 export async function GET() {
   try {
-   	return Response.json(await listInvoices());
+    const invoices = await listInvoices();
+    return Response.json(invoices);
   } catch (error) {
-  	return Response.json({ error }, { status: 500 });
+    return Response.json({ error: error instanceof Error ? error.message : error }, { status: 500 });
   }
 }
