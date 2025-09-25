798ae6a656bd18bc63fa4cf4d1e35542837e7149
Miguel Ángel López <mangell0624@gmail.com>
2025-09-25 10:16:13 +0200

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
@@ -17,11 +17,10 @@ async function getUser(email: string): Promise<User | undefined> {
     throw new Error('Failed to fetch user.');
   }
 }
- 
+
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [
-    Credentials({
+  providers: [Credentials({
       async authorize(credentials) {
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
@@ -31,10 +30,13 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
         }
- 
+
+        console.log('Invalid credentials');
         return null;
       },
     }),
-  ],
+],
 });
