12438755adfe4ff2dd44d6ee2b39987672004442
Miguel Ángel López <mangell0624@gmail.com>
2025-09-25 11:40:19 +0200

    Final metadata

--- auth.ts
+++ auth.ts
@@ -34,8 +34,6 @@ export const { auth, signIn, signOut } = NextAuth({
             const user = await getUser(email);
             if (!user) return null;
             const passwordsMatch = await bcrypt.compare(password, user.password);
-            console.log(user.email);
-            console.log(user.password);
             if (passwordsMatch) return user;
           }
          
