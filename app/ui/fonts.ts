691a11899d496b733f2692c2f951f83966af870b
Miguel Ángel López <mangell0624@gmail.com>
2025-09-29 13:04:35 +0200

    a

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,8 @@
-// Carga de fuentes
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google'; //Importamos las fuentes
  
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] }); // Preparamos la letra inter para usar solo caracters latinos.
 
-export const lusitana = Lusitana({
+export const lusitana = Lusitana({ // Exportar fuente ya configurada y puedes usar la normal de 400 o la mas gruesa de 700
   weight: ['400', '700'],
   subsets: ['latin'],
 });
