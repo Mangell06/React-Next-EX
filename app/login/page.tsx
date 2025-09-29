761c01c12524b7286038542a3ab8ac9fdf579b8c
Miguel Ángel López <mangell0624@gmail.com>
2025-09-29 12:21:20 +0200

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,11 +9,11 @@ export default function LoginPage() {
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
+            <AcmeLogo /> // Logo de Acme
           </div>
         </div>
         <Suspense> // Permite renderizado asincrono, eso significa que, puedes ver otras zonas que ya esten listas, mientras esperas que cargue esta.
-          <LoginForm />
+          <LoginForm /> // Contenido del Login
         </Suspense>
       </div>
     </main>
